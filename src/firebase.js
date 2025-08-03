cf26424429a018bc44e783f88cd1d5adc0d6ed23
florian-uh <flo.uhlinger@gmail.com>
2025-08-04 00:29:39 +0200

    040825

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCLO-dl4M7IIioktg7TKrKOOB38-eHZcYU",
-    authDomain: "blog-project-92404.firebaseapp.com",
-    projectId: "blog-project-92404",
-    storageBucket: "blog-project-92404.firebasestorage.app",
-    messagingSenderId: "198077257527",
-    appId: "1:198077257527:web:a064a3feda1fe3584facb2",
-    measurementId: "G-4JPR4JDEWZ"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export default app;
